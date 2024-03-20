a2bfed0760cea865759cd33a6f54a69e5c91d311
Mayuri <mayurigurav1997@gmail.com>
2024-03-20 20:24:59 +0530

    Updating the sidebar when add new project

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 
 const Input = forwardRef(({ label, textarea, ...props }, ref) => {
     const classes = "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
